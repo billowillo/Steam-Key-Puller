40a9b84a77c8d05a96274be91ef7d07186db05b1
billowillo <masonstorms@gmail.com>
2015-09-06 22:33:53 -0600

    Working on sendMessage

--- contentscript.js
+++ contentscript.js
@@ -1,9 +1,12 @@
-
-console.log("where do i go");
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-  console.log("Messsage"+ " " +msg+ " From"+sender);
-  if (msg.action == 'search') {
-    var pageHtml = document.documentElement.outerHTML;
-    sendResponse({data: "Wowzers"});
-  }
+chrome.runtime.onMessage.addListener(
+	function (request, sender){
+		if (request.line=='countparas'){ /* If we get the request from the Background script */
+			var paras = document.body.querySelectorAll('p'); /* Select all `<p>` elements in the document body */
+			if (paras.length > 0){ /* If the number of `<p>` elements is greater than zero */
+			var theCount = paras.length+''; /* Assigning that number to a variable called 'theCount' and convert it to a string format */
+			chrome.runtime.sendMessage({count:theCount}); /* Send the count back to the background script */
+			} else {
+				alert('There does not seem to be any `<p>` elements in this page!');
+			}
+		}
 });
